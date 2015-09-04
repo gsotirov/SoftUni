53e5eebeb41ced1c43340a0c313427445e00c89a
Georgi Sotirov - Cortez <gsotirov88@gmail.com>
2015-09-04 12:43:15 +0300

    Fixes and tasks added
    ...

---
+++ SoftUni_Homework__Primitive_Data_Types_and_Variables/Problem_18_NineDigit_Magic_Number/NineDigitMagicNumber.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Problem_18_NineDigit_Magic_Number
+{
+	class NineDigitMagicNumber
+	{
+		public static void Main ()
+		{
+			int sum = int.Parse(Console.ReadLine());
+			int diff = int.Parse(Console.ReadLine());
+			int count = 0;
+			int s = 111;
+			int e = 777;
+			int firstNumber = s;
+
+			while (firstNumber <= e) 
+			{
+				int secondNumber = diff + firstNumber;
+				int thirdNumber = diff + secondNumber;
+
+				int firstSum = CalculateDigitSum (firstNumber);
+				int secondSum = CalculateDigitSum (secondNumber);
+				int thirdSum = CalculateDigitSum (thirdNumber);
+
+				// Add some checks here...
+				bool checkThirdNum = thirdNumber <= e;
+				bool checkSum = (firstSum + secondSum + thirdSum) == sum;
+				bool checkDigits = CheckIfDigitsAllowed (firstNumber) && CheckIfDigitsAllowed (secondNumber) && CheckIfDigitsAllowed (thirdNumber);
+
+				// Check if the number meet the requirements...
+
+				if (checkDigits && checkThirdNum && checkSum)
+				{
+					// If they DO, print them on the console...
+					Console.WriteLine ("{0}{1}{2}", firstNumber, secondNumber, thirdNumber);
+					count++;
+				}
+				firstNumber++;
+			}
+			if (count == 0) 
+			{
+				Console.WriteLine ("No");
+			}
+		}
+
+		static bool CheckIfDigitsAllowed (int number)
+		{
+			bool allowed = true;
+			int len = number.ToString().Length;
+
+			for (int i = 0; i < len; i++) 
+			{
+				int digit = number % 10;
+				if (digit < 1 || digit > 7)
+				{
+					allowed = false;
+				}
+				number /= 10;
+			}
+
+			return allowed;
+		}
+
+		static int CalculateDigitSum (int number)
+		{
+			int sum = 0;
+			while(number > 0)
+			{
+				sum += number % 10;
+				number /= 10;
+			}
+			return sum;
+		}
+	}
+}
---
+++ SoftUni_Homework__Primitive_Data_Types_and_Variables/Problem_19_Bits_Inverter/BitsInverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Problem_19_Bits_Inverter
+{
+	class BitsInverter
+	{
+		public static void Main ()
+		{
+			sbyte n = sbyte.Parse (Console.ReadLine());
+			sbyte step = sbyte.Parse (Console.ReadLine());
+
+			string bitsStr = "";
+
+			for (int i = 0; i < n; i++) 
+			{
+				string byteStr = Convert.ToString(int.Parse (Console.ReadLine()), 2);
+				if (byteStr.Length < 8) 
+				{
+					// Add 0s to bytes where necessary so they are with the proper length(8).
+					byteStr = byteStr.PadLeft (8, '0');
+				}
+				bitsStr += byteStr;
+			}
+
+			int len = bitsStr.Length;
+			string output = "";
+
+
+			for (int j = 0; j < len; j++) 
+			{
+				char digit = bitsStr[j];
+
+				if (j == 0 || j % step == 0) 
+				{
+					digit = (digit == '0') ? '1' : '0';
+				}
+
+				output += digit;
+
+				if (output.Length == 8) 
+				{
+					Console.WriteLine(Convert.ToInt32(output, 2));
+					output = "";
+				}
+			}
+		}
+	}
+}
